13c6a50aa3fb591574f6d52914ae88a00ef0b9f1
Santiago Pérez <rivenvirus@gmail.com>
2016-10-17 12:00:27 -0500

    Version 0.0.3

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # duplicaText Tool 
 ## Is a simple tool to duplicate a selected text on VSCode editor
 
+Last changes:
+- Allow dupicate Text on Multicursor
+- Fix text selection on duplicate
+
 Set the Keybindings
  ```
 [
--- src/extension.ts
+++ src/extension.ts
@@ -7,9 +7,16 @@ export function activate() {
         const editor = vscode.window.activeTextEditor;
 		const selection = editor.selection;
         if(!selection.isEmpty){
+            var selections = [];
+            editor.selections.forEach(function(item){
+                var selection_count = (item.end.character-item.start.character);
+                var new_selection = new vscode.Selection(item.start.line,(item.start.character+selection_count),item.end.line,(item.end.character+selection_count))
+                selections.push(new_selection);
+            });
             vscode.commands.executeCommand("editor.action.clipboardCopyAction");
             vscode.commands.executeCommand("editor.action.clipboardPasteAction");
             vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+            vscode.window.activeTextEditor.selections = selections;
         }
 	});
 }
